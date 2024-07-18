f9eea60879285a4b70c43774c8ad98d9cbea5f41
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-07-18 16:32:10 +0530

    transaction data model added

---
+++ server/models/Transaction.js
@@ -0,0 +1,25 @@
+import {Schema, model} from "mongoose"
+
+//credit - money comes in
+//debit - money go out
+
+const transactionSchema = new Schema({
+    amount : {
+        type : Number,
+        required : true,
+    },
+    category : {
+        type : String,
+        default : "Others",
+    },
+    type : {
+        type : String,
+        required : true,
+    },
+    user: {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+    }
+})
+
+export default transactionSchema;
