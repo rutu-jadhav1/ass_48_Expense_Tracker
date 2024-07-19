2aa991aafd481c2553a5d0241f9cc77e31d8510a
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-07-19 15:52:30 +0530

    transaction card css

--- client/src/components/TransactionCard/TransactionCard.js
+++ client/src/components/TransactionCard/TransactionCard.js
@@ -5,6 +5,15 @@ function TransactionCard({_id, title, amount, category, type, createdAt}) {
   return (
     <div className='transaction-card'>
         <h1 className='transaction-card-title'> {title}</h1>
+
+        <span className='transaction-card-date'>
+          {new Date(createdAt).toLocaleString()}
+        </span>
+
+        <span className='transaction-card-category'>
+          {category}
+        </span>
+
         <span className='transaction-card-amount'style={{
           color : type === "credit" ? "green" : "red"
         }}>
