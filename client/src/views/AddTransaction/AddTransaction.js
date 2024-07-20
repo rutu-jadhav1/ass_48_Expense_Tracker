865b80fe68aaf5d612ba72fefc4ca3e7abd483f9
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-07-20 09:24:32 +0530

    add transaction completed

--- client/src/views/AddTransaction/AddTransaction.js
+++ client/src/views/AddTransaction/AddTransaction.js
@@ -1,27 +1,70 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import toast, {Toaster} from 'react-hot-toast'
 import "./AddTransaction.css"
 
+
 function AddTransaction() {
+  const [user, setUser] =useState('')
+  const [title, setTitle] = useState('')
+  const [amount, setAmount] = useState('0')
+  const [type, setType] = useState('credit')
+  const [category, setCategory] = useState('learning')
+
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'))
+
+    if (currentUser) {
+      setUser(currentUser)
+    }
+
+    if (!currentUser) {
+      window.location.href = '/login'
+    }
+  }, [])
+
+  const addTransaction = async () => {
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/transaction`, {
+      title,amount,type,category,user : user._id
+    })
+    toast.success(response.data.message)
+    setTitle('')
+    setAmount('')
+    setType('')
+    setCategory('')
+    setTimeout(() => {
+      window.location.href = "/"
+    }, 2000)
+    
+  }
+
   return (
     <div>
-        <h1 className="auth-heading">Add Transaction</h1>
+        <h1 className="auth-heading">Add Transaction for {user.fullName}</h1>
         <form className="auth-form">
         <input
           type="text"
           placeholder="Enter Title"
           className="user-input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <input
           type="number"
           placeholder="Enter Amount"
           className="user-input"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
         />
-        <select className='user-input'>
+        <select className='user-input' 
+          value={type}
+          onChange={(e) => setType(e.target.value)}>
             <option value='credit'>Income</option>
             <option value='debit'>Expense</option>
         </select>
-        <select className='user-input'>
+        <select className='user-input'
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}>
             <option value="food">Food</option>
             <option value="expense">Expense</option>
             <option value="rent">Rent</option>
@@ -34,10 +77,11 @@ function AddTransaction() {
             <option value="salary">Salary</option>
         </select>
     
-        <button type='button' className='btn-auth'>
+        <button type='button' className='btn-auth' onClick={addTransaction}>
             Add Transaction
         </button>
         </form>
+        <Toaster/>
     </div>
   )
 }
